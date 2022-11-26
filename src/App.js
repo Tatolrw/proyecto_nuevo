8c51983aaf0710efbdae857e97525fc4d85764eb
Tatolrw <116382855+Tatolrw@users.noreply.github.com>
2022-11-26 17:22:57 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<div className="container">
+  <h1 className="title">
+     Base de datos de pacientes UMAE <span aria-label="emoji" role="img">🔥</span>
+  </h1>
+  <br></br>
+  <h2>
+    Tareas
+    </h2>
+  <input type="text" className="new-task"/>
+  <br></br>
+  <input type="text" className="new-task"/>
+  <br></br>
+  <input type="text" className="new-task"/>
+  <br></br>
+  <input type="text" className="new-task"/>
+  <br></br>
+  <input type="text" className="new-task"/>
+  <caja1 className="new-task">hola nigga</caja1>
+</div>
+
+  );
+}
+
+export default App;
