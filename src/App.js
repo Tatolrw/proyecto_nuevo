0ee8b0bc000472db9894ab80604a2a99360b7365
Tatolrw <116382855+Tatolrw@users.noreply.github.com>
2022-11-26 18:49:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (<div className="container">
   <h1 className="title">
-     Base de datos de pacientes UMAE <span aria-label="emoji" role="img">🔥</span>
+     Base de datos de pacientes UMAE <span aria-label="emoji" role="img">💉</span>
   </h1>
   <br></br>
   <h2>
-    Tareas
+    Bienvenido!
     </h2>
+    <p>Ingrese su numero de paciente. </p>
   <input type="text" className="new-task"/>
   <br></br>
+  <p>Correo electronico </p>
   <input type="text" className="new-task"/>
   <br></br>
+  <p>Contraseña </p>
   <input type="text" className="new-task"/>
   <br></br>
-  <input type="text" className="new-task"/>
   <br></br>
-  <input type="text" className="new-task"/>
-  <caja1 className="new-task">hola nigga</caja1>
+  <br></br>
+  <button onclick="activateLasers()" link="https://github.com/Tatolrw/proyecto_nuevo">
+  Iniciar Sesion
+</button>
+
 </div>
 
   );
